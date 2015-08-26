df4407579dc4c048cad9ecf23ddafa3b48520079
SVRajasekhar <surevenkatarajasekhar@gmail.com>
2015-08-26 12:37:39 +0530

    xbee.c

--- Sensor/ReedSensor/xbee.c
+++ Sensor/ReedSensor/xbee.c
@@ -14,24 +14,31 @@
 void xbee_init()
 {
 	serial_init();
+	return;
 }
 
 
 
 bool xbee_send(const uint8_t *buf, uint16_t len, uint64_t address)
 {
-	// Send transmit request
-	serial_send(0x7e);	// Start delimiter
 	uint16_t totalLen = len + 14;
-	serial_send(GET_BYTE(totalLen, 0));	// Length MSB
-	serial_send(GET_BYTE(totalLen, 1));	// Length LSB
 	static uint8_t frameID = 1;
 	uint8_t checksum = 0x10 + frameID + 0xff + 0xfe + 0x00 + 0x00;
+	uint8_t value,SerReceiveData;
+	uint16_t i,indx,idx;
+	bool exitLoop, success;
+	// Send transmit request
+	serial_send(0x7e);	// Start delimiter
+	
+	serial_send(GET_BYTE(totalLen, 0));	// Length MSB
+	serial_send(GET_BYTE(totalLen, 1));	// Length LSB
+	
+	
 	serial_send(0x10);	// Frame type: ZigBee Transmit Request
 	serial_send(frameID);	// Frame ID
-	for (uint16_t i = 0; i < 8; i++)
+	for ( indx = 0; indx < 8; indx++)
 	{
-		uint8_t value = GET_BYTE(address, i); // 64 bit destination address: MSB..LSB
+		value = GET_BYTE(address, i); // 64 bit destination address: MSB..LSB
 		serial_send(value);
 		checksum += value;
 	}
@@ -39,7 +46,8 @@ bool xbee_send(const uint8_t *buf, uint16_t len, uint64_t address)
 	serial_send(0xfe);	// 16-bit destination address LSB
 	serial_send(0x00);	// Broadcast radius
 	serial_send(0x00);	// Options
-	for (uint16_t i = 0; i < len; i++)
+	if
+	for (i = 0; i < len; i++)
 	{
 		serial_send(buf[i]);	// RF data
 		checksum += buf[i];
@@ -47,25 +55,26 @@ bool xbee_send(const uint8_t *buf, uint16_t len, uint64_t address)
 	serial_send(0xff - checksum);	// Checksum
 
 	// Receive frames util we get the transmit status 
-	bool exitLoop, success;
+	
 	while (true)
 	{
 		// Omit all bytes before frame delimiter
 		while (serial_receive() != 0x7e);
-		exitLoop = true; success = true;	// Be optimistic ...
+		exitLoop = true; 
+		success = true;	// Be optimistic ...
 		totalLen = 0;
 		SET_BYTE(totalLen, 0, (uint16_t)serial_receive());	// Length MSB
 		SET_BYTE(totalLen, 1, (uint16_t)serial_receive());	// Length LSB
 		if (totalLen != 7)	// Size of answer
 			exitLoop = false;
-		for (uint16_t i = 0; i < totalLen; i++)
+		for ( idx = 0; idx < totalLen; i++)
 		{
-			uint8_t value = serial_receive();
-			if ((i == 0) && (value != 0x8b))	// Frame type: ZigBee Transmit Status
+			SerReceiveData = serial_receive();
+			if ((idx == 0) && (value != 0x8b))	// Frame type: ZigBee Transmit Status
 				exitLoop = false;
-			if ((i == 1) && (value != frameID))	// Correct Frame ID
+			if ((idx == 1) && (value != frameID))	// Correct Frame ID
 				exitLoop = false;
-			if (i == 5)
+			if (idx == 5)
 				success = (value == 0); // Delivery Status: OK
 		}
 		serial_receive();	// Checksum
