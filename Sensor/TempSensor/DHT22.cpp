fd8b0a671a654b24344e013d3495d5e22fb8315d
SVRajasekhar <surevenkatarajasekhar@gmail.com>
2015-09-12 14:20:06 +0530

    DHT22.cpp
    Exception Handlers

--- Sensor/TempSensor/DHT22.cpp
+++ Sensor/TempSensor/DHT22.cpp
@@ -1,16 +1,37 @@
 #include "DHT22.h"
-
-
+#include <vector>
+#include <stdio>
+using namespace std;
 
 DHT22::DHT22(uint8_t powerPin, uint8_t ioPin)
 {
 	this->powerPin = powerPin;
+	try{
 	pinMode(powerPin, OUTPUT);
+	}catch(Exception& e){
+        cout << "Exception caught" << std::endl;
+        cout << e.what() << std::endl;
+        }
+	try{
 	digitalWrite(powerPin, LOW);
+	catch(Exception& e){
+        cout << "Exception caught" << std::endl;
+        cout << e.what() << std::endl;
+        }
 
 	this->ioPin = ioPin;
+	try{
 	pinMode(ioPin, INPUT);
+	catch(Exception& e){
+        cout << "Exception caught" << std::endl;
+        cout << e.what() << std::endl;
+        }
+	try{
 	digitalWrite(ioPin, LOW);
+        catch(Exception& e){
+        cout << "Exception caught" << std::endl;
+        cout << e.what() << std::endl;
+        }
 }
 
 
@@ -22,23 +43,49 @@ bool DHT22::Read(int16_t &temperature, int16_t &humidity)
 	uint8_t j = 0;
 
 	// Receive buffer
-	uint8_t data[6];
-	data[0] = data[1] = data[2] = data[3] = data[4] = 0;
+	uint8_t data[6]; 
+	vector<uint8_t> v_DHT22data(data);
+	v_DHT22data[0] = v_DHT22data[1] = v_DHT22data[2] = v_DHT22data[3] = v_DHT22data[4] = 0;
 
 	// Turn on power
+	try{
 	digitalWrite(powerPin, HIGH);
+	}catch(Exception& e){
+        cout << "Exception caught" << std::endl;
+        cout << e.what() << std::endl;
+        }
 	delay(1000);
 
 	// Pull the pin high and wait 250 milliseconds
+	try{
 	digitalWrite(ioPin, HIGH);
+	}catch(Exception& e){
+        cout << "Exception caught" << std::endl;
+        cout << e.what() << std::endl;
+        }
 	delay(250);
 
 	// Now pull it low for ~20 milliseconds
+	try{
 	pinMode(ioPin, OUTPUT);
+	}catch(Exception& e){
+        cout << "Exception caught" << std::endl;
+        cout << e.what() << std::endl;
+        }
+        try{
 	digitalWrite(ioPin, LOW);
+        }catch(Exception& e){
+        cout << "Exception caught" << std::endl;
+        cout << e.what() << std::endl;
+        }
 	delay(20);
 	noInterrupts();
+	try{
 	digitalWrite(ioPin, HIGH);
+	}catch(Exception& e){
+        cout << "Exception caught" << std::endl;
+        cout << e.what() << std::endl;
+        }
 	delayMicroseconds(40);
 	pinMode(ioPin, INPUT);
 
@@ -70,17 +117,27 @@ bool DHT22::Read(int16_t &temperature, int16_t &humidity)
 	interrupts();
 
 	// Turn off power
+	try{
 	digitalWrite(ioPin, LOW);
+	}catch(Exception& e){
+        cout << "MyException caught" << std::endl;
+        cout << e.what() << std::endl;
+        }
+	try{
 	digitalWrite(powerPin, LOW);
+	}catch(Exception& e){
+        cout << "MyException caught" << std::endl;
+        cout << e.what() << std::endl;
+        }
 
 	if (j != 40)	// 40 bits = 5 bytes read?
 		return false;
-	if (data[4] != ((data[0] + data[1] + data[2] + data[3]) & 0xFF)) // CRC
+	if (v_DHT22data[4] != ((v_DHT22data[0] + v_DHT22data[1] + v_DHT22data[2] + v_DHT22data[3]) & 0xFF)) // CRC
 		return false;
 
-	humidity = (data[0] << 8) | data[1];
-	temperature = ((data[2] & 0x7F) << 8) |  data[3];
-	if (data[2] & 0x80)
+	humidity = (v_DHT22data[0] << 8) | v_DHT22data[1];
+	temperature = ((v_DHT22data[2] & 0x7F) << 8) |  v_DHT22data[3];
+	if (v_DHT22data[2] & 0x80)
 		temperature = -temperature;
 
 	return true;
